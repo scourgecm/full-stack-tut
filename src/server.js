f5c949d64e2737f5d4a2fe294bc1a4c6321f59ee
pcornejo <pcornejo@opengets.cl>
2018-03-14 17:24:46 -0300

    Completed! : - Broadcasting State from A Redux Listener - Receiving Remote Redux Actions

--- src/server.js
+++ src/server.js
@@ -2,4 +2,13 @@ import Server from "socket.io";
 
 export function startServer() {
     const io = new Server().attach(3000);
+
+    store.subscribe(
+        () => io.emmit('state', store.getState().toJS())
+    );
+
+    io.on('connection', (socket) => {
+        socket.emit('state', store.getState().toJS());
+        socket.on('action', store.dispatch.bind(store));
+    });
 }
